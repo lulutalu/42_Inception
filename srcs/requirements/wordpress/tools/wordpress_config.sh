9f80238d27007d70859cc7b21f8235be846f91bf
Loic Dubouloz <lulutalu@gmail.com>
2023-07-09 18:21:29 +0200

    Working on localhost need to import it inside a VM

--- srcs/requirements/wordpress/tools/wordpress_config.sh
+++ srcs/requirements/wordpress/tools/wordpress_config.sh
@@ -1,19 +1,26 @@
 #!/bin/ash
 
-while ! mariadb -h mariadb -u "$DB_USER" -p"$DB_PASS" "$DB_NAME"; do
+while ! mysql -h mariadb -u $DB_USER -p$DB_PASS $DB_NAME; do
   sleep 3
 done
+
+sed -i "s|^listen = .*$|listen = 0.0.0.0:9000|" /etc/php81/php-fpm.d/www.conf
+sed -i "s|^;cgi.fix_pathinfo=1|cgi.fix_pathinfo=0|" /etc/php81/php.ini
+
+echo "Connection successful with db"
 
 cd /var/www/wordpress
 
 wp core download
 
-wp config create --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASS}" \
-		--dbhost=mariadb --locale=en_US --skip-check
+wp core config --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASS}" \
+		--dbhost=mariadb --locale=en_US --debug=true;
 
 wp core install --url="${WP_URL}" --title="${WP_TITLE}" --admin_user="${WP_ADMINUSER}" \
 	--admin_password="${WP_ADMINPASS}" --admin_email="${WP_ADMINEMAIL}" --locale=en_US --skip-email
 
-wp user create "${WP_USER}" "${WP_USERMAIL}" --user_pass="${WP_USERPASS}" --role=author
+wp plugin update --all
+
+wp user create "${WP_USER}" "${WP_USEREMAIL}" --user_pass="${WP_USERPASS}" --role=author
 
 exec /usr/sbin/php-fpm81 -F -O
