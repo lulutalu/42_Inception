91712c78dee55e175c6ba7dc1a188c7193e22b3a
Loic Dubouloz <lulutalu@gmail.com>
2023-07-11 19:37:15 +0200

    Corrected Nginx to point the wordpress to custom domain_name

--- srcs/requirements/nginx/tools/nginx_conf.sh
+++ srcs/requirements/nginx/tools/nginx_conf.sh
@@ -1,5 +1,8 @@
 #!/bin/ash
 
+
+sed -i "s/^ server_name DOMAIN_NAME;$/	server_name $DOMAIN_NAME;/" /etc/nginx/http.d/nginx.conf
+
 openssl req -x509 -newkey rsa:4096 \
 		-keyout /etc/ssl/key.pem \
 		-out /etc/ssl/fullchain.pem \
